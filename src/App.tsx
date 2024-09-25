892d789b1289bd9f3e0083f65de16922436897b8
Leonardo <leonardomenezes47@outlook.com>
2024-09-25 16:39:57 -0400

    finish responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,29 @@ export function App() {
     		<h1 className="text-xl font-bold text-primary">upload.ai</h1>
 
 				<div className="flex items-center gap-3">
-					<span className="text-sm text-muted-foreground">
+					<span className="text-sm text-muted-foreground max-md:hidden">
 						Desenvolvido por Ydev
 					</span>
 
 					<Separator orientation="vertical" className="h-6"/>
 
 					<Button variant="outline">
-						<Instagram className="w-4 h-4 mr-2"/>
-						Instagram
+						<Instagram className="w-4 h-4 mr-2 max-md:mr-0 max-md:w-6 max-md:h-6"/>
+						<span className="max-md: hidden">Instagram</span>
 					</Button>
 				</div>
    		</div>
 
-			<main className="flex-1 p-6 flex gap-6">
+			<main className="flex-1 p-6 flex gap-6 max-md:flex-col max-md:items-center">
 				<div className="flex flex-col flex-1 gap-4">
 					<div className="grid grid-rows-2 gap-4 flex-1">
 						<Textarea 
-							className="resize-none p-4 leading-relaxed focus:outline-none focus:border-gray-600 focus:border-[2px]"
+							className="resize-none p-4 leading-relaxed focus:outline-none focus:border-gray-600 focus:border-[2px] max-md:h-64"
 							placeholder="Inclua o prompt para a IA..."
 						/>
 
 						<Textarea 
-							className="resize-none p-4 leading-relaxed focus:outline-none focus:border-gray-600 focus:border-[2px]"
+							className="resize-none p-4 leading-relaxed focus:outline-none focus:border-gray-600 focus:border-[2px] max-md:h-64"
 							placeholder="Resultado gerado pela IA..." 
 							readOnly
 						/>
@@ -47,7 +47,7 @@ export function App() {
 					</p>
 				</div>
 
-				<aside className="w-80 space-y-6 overflow-auto">
+				<aside className="w-80 space-y-6 overflow-auto max-md:w-auto">
 					<form className="space-y-6">
 						<label 
 							htmlFor="video"
